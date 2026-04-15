0f70a1e22a0cda34097c4302fb7162f1468eb126
akhil <akhilt.eai@svam.com>
2026-04-15 17:42:32 +0530

    changes for concerns

--- app/models/concerns/payable.rb
+++ app/models/concerns/payable.rb
@@ -1,17 +1,15 @@
 module Payable
-    extend ActiveSupport::Concern
-  
-    # ⚠️ Do NOT define has_many here unless ALL models need it
-  
-    def total_paid
-      payments.where(status: :completed).sum(:amount)
-    end
-  
-    def outstanding_amount
-      rent_amount - total_paid
-    end
-  
-    def fully_paid?
-      outstanding_amount <= 0
-    end
+  extend ActiveSupport::Concern
+
+  def total_paid
+    payments.where(status: :completed).sum(:amount)
   end
+
+  def outstanding_amount
+    rent_amount - total_paid
+  end
+
+  def fully_paid?
+    outstanding_amount <= 0
+  end
+end
